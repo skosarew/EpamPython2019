8cc9ae29a88695262d336f4788e7c7aa7b076b66
Evgeny <skosarew@mail.ru>
2019-12-11 16:19:16 +0300

    HW_7: alpha ver.

--- 07-python-for-advanced/hw/task2.py
+++ 07-python-for-advanced/hw/task2.py
@@ -7,17 +7,68 @@
 """
 
 import time
+import datetime
 import uuid
+
+
+def timer_property(t):
+    ttl = datetime.timedelta(seconds=t)
+
+    """Caches the curr_time of function in memory with time to live"""
+
+    class custom_property(object):
+        def __init__(self, fget=None, fset=None, fdel=None, doc=None):
+            self.fget = fget
+            self.fset = fset
+            self.fdel = fdel
+            self.curr_time = 0
+            self.result = 0
+            if doc is None and fget is not None:
+                doc = fget.__doc__
+            self.__doc__ = doc
+
+        def __get__(self, obj, objtype=None):
+            if obj is None:
+                return self
+            if self.fget is None:
+                raise AttributeError("unreadable attribute")
+            if self.curr_time == 0 or \
+                    datetime.datetime.now() - self.curr_time > ttl:
+                self.result = self.fget(obj)
+                self.curr_time = datetime.datetime.now()
+            return self.result
+
+        def __set__(self, obj, value):
+            if self.fset is None:
+                raise AttributeError("can't set attribute")
+            self.curr_time = datetime.datetime.now()
+            self.result = value
+
+        def __delete__(self, obj):
+            if self.fdel is None:
+                raise AttributeError("can't delete attribute")
+            self.fdel(obj)
+
+        def getter(self, fget):
+            return type(self)(fget, self.fset, self.fdel, self.__doc__)
+
+        def setter(self, fset):
+            return type(self)(self.fget, fset, self.fdel, self.__doc__)
+
+        def deleter(self, fdel):
+            return type(self)(self.fget, self.fset, fdel, self.__doc__)
+
+    return custom_property
 
 
 class Message:
 
-    @timer_property(t=10)
+    @timer_property(t=2)
     def msg(self):
         self._msg = self.get_message()
         return self._msg
 
-    @msg.setter # reset timer also
+    @msg.setter  # reset timer also
     def msg(self, param):
         self._msg = param
 
@@ -25,12 +76,20 @@ class Message:
         """
         Return random string
         """
-        return uuid.uuid4().get_hex()
+        return uuid.uuid4().hex
 
 
 if __name__ == '__main__':
+    # m = Message()
+    # initial = m.msg
+    # assert initial is m.msg
+    # time.sleep(2)
+    # assert initial is not m.msg
+
     m = Message()
-    initial = m.read
-    assert initial is m.read
-    time.sleep(10)
-    assert initial is not m.read
+    m.msg = '12345qwerty'
+    final = m.msg
+    time.sleep(2.2)
+    del m.msg
+    print(m.msg)
+    assert final is not m.msg
--- 07-python-for-advanced/hw/task3.py
+++ 07-python-for-advanced/hw/task3.py
@@ -2,3 +2,93 @@
 Написать тесты(pytest or unittest) к предыдущим 2 заданиям, запустив которые, я бы смог бы проверить их корректность
 Обязательно проверить всю критическую функциональность
 """
+
+import unittest
+from task2 import Message
+from task1 import SiamObj
+import time
+
+
+class TestMessage(unittest.TestCase):
+    def test_same_msg(self):
+        """Returns True if msg is the same
+        """
+        m = Message()
+        initial = m.msg
+        self.assertEqual(initial, m.msg)
+
+    def test_msg_with_delay(self):
+        """Returns True if msg is not the same
+        """
+        m = Message()
+        initial = m.msg
+        time.sleep(2)
+        self.assertNotEqual(initial, m.msg)
+
+    def test_msg_with_small_delay(self):
+        """Returns True if msg is not the same
+        """
+        m = Message()
+        initial = m.msg
+        time.sleep(1.9)
+        self.assertEqual(initial, m.msg)
+
+    def test_msg_set(self):
+        """Returns True if msg is the same
+        """
+        m = Message()
+        m.msg = '12345qwerty'
+        final = m.msg
+        # print(final, m.msg)
+        self.assertEqual(final, m.msg)
+
+    def test_msg_set_with_delay(self):
+        """Returns True if msg is not the same
+        """
+        m = Message()
+        m.msg = '12345qwerty'
+        final = m.msg
+        time.sleep(2)
+        self.assertNotEqual(final, m.msg)
+
+
+class TestMeta(unittest.TestCase):
+    def test_siam_equal(self):
+        """Returns True if objects with same arguments is the same
+        """
+        unit1 = SiamObj('1', '2', a=1)
+        unit2 = SiamObj('1', '2', a=1)
+        self.assertEqual(unit1, unit2)
+
+    def test_siam_not_equal(self):
+        """Returns True if objects with different args isn't the same
+        """
+        unit1 = SiamObj('1', '2', a=1)
+        unit3 = SiamObj('2', '2', a=1)
+        self.assertNotEqual(unit1, unit3)
+
+    def test_siam_connections(self):
+        """Returns True if object can access other objects
+        of the same class
+        """
+        unit1 = SiamObj('1', '2', a=1)
+        unit2 = SiamObj('1', '2', a=1)
+        unit3 = SiamObj('2', '2', a=1)
+        unit3.connect('1', '2', 1).a = 2
+        self.assertEqual(2, unit1.a)
+        self.assertEqual(2, unit2.a)
+
+    def test_siam_deletion(self):
+        """Return True if after deletion object the pool decreases by 1
+        """
+        unit1 = SiamObj('1', '2', a=1)
+        unit2 = SiamObj('1', '2', a=1)
+        unit3 = SiamObj('2', '2', a=1)
+        pool = unit3.pool
+        self.assertEqual(2, len(pool))
+        del unit3
+        self.assertEqual(1, len(pool))
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False, verbosity=3)
