3574bdf7bbd0781bc12779061abb75d914bd0b7c
Evgeny <skosarew@mail.ru>
2019-12-26 07:05:20 +0300

    HW_12 fix SOLID2

--- 12-object-oriented-design/hw_transportation.py
+++ 12-object-oriented-design/hw_transportation.py
@@ -6,11 +6,29 @@ class Building:
     """Framework for buildings"""
 
     def __init__(self):
-        self.vehicles = {}
+        self.vehicles = []
 
     def add_vehicles(self, *args):
         for arg in args:
-            self.vehicles[arg] = 0
+            self.vehicles.append(arg)
+
+    def attach_cargo(self, cargo):
+        self.stock.append(cargo)
+
+    def detach_cargo(self):
+        self.stock.popleft()
+
+    @property
+    def is_stock(self):
+        return self.stock
+
+    def find_min(self):
+        min_val, vehicle = float('inf'), None
+        for v in self.vehicles:
+            if v.travel_time < min_val:
+                min_val = v.travel_time
+                vehicle = v
+        return vehicle
 
 
 class Factory(Building):
@@ -19,6 +37,23 @@ class Factory(Building):
     def __init__(self, stock):
         self.stock = deque([i, 0] for i in stock)
         super(Factory, self).__init__()
+
+    def transportation(self, travel):
+        cargo = self.stock.popleft()
+        address = Warehouse.warehouses[cargo[0]]
+        vehicle = self.find_min()
+
+        # If there is port on the way
+        if address.port:
+            cargo[1] += address.port.travel_time + vehicle.travel_time
+            vehicle.travel_time += address.port.travel_time * 2
+            address.port.attach_cargo(cargo)
+
+            # Change to ship
+            vehicle = address.port.find_min()
+            travel.send_vehicle_from_port(cargo, address, vehicle)
+        else:
+            travel.send_vehicle_from_factory(cargo, address, vehicle)
 
 
 class Port(Building):
@@ -41,88 +76,86 @@ class Warehouse:
         self.port = port
         Warehouse.warehouses[name] = self
 
+    @property
+    def has_port(self):
+        return self.port
+
+    def attach_cargo(self, cargo):
+        self.stock.append(cargo)
+
+
+class Travelling:
+    def __init__(self, factory, *warehouses):
+        self.factory = factory
+        self.warehouses = warehouses
+
+    # @staticmethod
+    def send_vehicle_from_port(self, cargo, address, vehicle):
+        if cargo[1] < vehicle.travel_time:
+            cargo[1] = address.travel_time + vehicle.travel_time
+            vehicle.travel_time += address.travel_time * 2
+        else:
+            vehicle.travel_time = cargo[1] + address.travel_time * 2
+            cargo[1] += address.travel_time
+
+        address.attach_cargo(cargo)
+        address.port.detach_cargo()
+
+        while address.port.is_stock:
+            address.port.transportation(cargo, address)
+
+    # @staticmethod
+    def send_vehicle_from_factory(self, cargo, address, vehicle):
+        """Sends vehicle from port to deliver containers."""
+        if cargo[1] < vehicle.travel_time:
+            cargo[1] = address.travel_time + vehicle.travel_time
+            vehicle.set_travel_time(address.travel_time * 2)
+
+        # Ready to take cargo immediately
+        else:
+            vehicle.set_travel_time(cargo[1] + address.travel_time * 2)
+            cargo[1] += address.travel_time
+        address.attach_cargo(cargo)
+        logging.info(cargo)
+
 
 class Transport:
     """Vehicle to transport containers."""
 
     def __init__(self):
         self.cargo = deque()
+        self.travel_time = 0
 
-
-def transportation(building):
-    """Returns cargo, address to deliver and vehicle who will transport"""
-    cargo = building.stock.popleft()
-    address = Warehouse.warehouses[cargo[0]]
-    vehicle = min(building.vehicles, key=building.vehicles.get)
-    return cargo, address, vehicle
-
-
-def send_vehicle_from_port(port):
-    """Sends vehicle from port to deliver containers."""
-    cargo, address, vehicle = transportation(port)
-
-    if cargo[1] < port.vehicles[vehicle]:
-        cargo[1] = address.travel_time + port.vehicles[vehicle]
-        port.vehicles[vehicle] += address.travel_time * 2
-    else:
-        port.vehicles[vehicle] = cargo[1] + address.travel_time * 2
-        cargo[1] += address.travel_time
-    address.stock.append(cargo)
-    logging.info(cargo)
-
-
-def send_vehicle(factory):
-    """Sends vehicle from factory to deliver containers."""
-    while factory.stock:
-        cargo, address, vehicle = transportation(factory)
-
-        # If there is port on the way
-        if address.port:
-            cargo[1] += address.port.travel_time + factory.vehicles[vehicle]
-            factory.vehicles[vehicle] += address.port.travel_time * 2
-            address.port.stock.append(cargo)
-            while address.port.stock:
-                send_vehicle_from_port(address.port)
-        else:
-            # If vehicle still in on the way add to travel time of cargo
-            # journey time of vehicle
-            if cargo[1] < factory.vehicles[vehicle]:
-                cargo[1] = address.travel_time + factory.vehicles[vehicle]
-                factory.vehicles[vehicle] += address.travel_time * 2
-
-            # Ready to take cargo immediately
-            else:
-                factory.vehicles[vehicle] = cargo[1] + address.travel_time * 2
-                cargo[1] += address.travel_time
-            address.stock.append(cargo)
-            logging.info(cargo)
+    def set_travel_time(self, time):
+        self.travel_time += time
 
 
 if __name__ == '__main__':
     inp = input('Enter container sequence: ')
-
     logging.basicConfig(filename="log_info.log",
                         filemode="w", level=logging.INFO)
     logging.info("Program started")
 
     port1 = Port(1)
-    port2 = Port(1)
+
     warehouse_a = Warehouse('A', 4, port=port1)
     warehouse_b = Warehouse('B', 5, port=None)
 
-    factory = Factory(inp)
+    factory1 = Factory(inp)
 
     truck1 = Transport()
     truck2 = Transport()
 
     ship_a = Transport()
-    ship_b = Transport()
 
-    factory.add_vehicles(truck1, truck2)
+    factory1.add_vehicles(truck1, truck2)
     port1.add_vehicles(ship_a)
-    port2.add_vehicles(ship_b)
 
-    send_vehicle(factory)
+    travel = Travelling(factory1, warehouse_a, warehouse_b)
+
+    while factory1.stock:
+        factory1.transportation(travel)
+
     max_a = max(warehouse_a.stock)[1]
     max_b = max(warehouse_b.stock)[1]
     time = max(max_a, max_b)
@@ -130,3 +163,6 @@ if __name__ == '__main__':
     logging.info(f"Number of hours that it would take to get containers "
                  f"delivered: {time} ")
     logging.info("Finished")
+    print(factory1.stock)
+    print(warehouse_a.stock)
+    print(warehouse_b.stock)
